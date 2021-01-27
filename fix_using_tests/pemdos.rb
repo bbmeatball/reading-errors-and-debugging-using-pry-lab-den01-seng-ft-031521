f46a5352bd5344794d77674b1971b9290b9ce5a1
JoAnn Siciliano <joannsiciliano@gmail.com>
2021-01-27 01:28:18 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,7 +1,7 @@
 # don't forget to add: require 'pry'
 def snake_it_up(string)
   string= "surprise"
-  if string == "surprise"
+  if string == "surprise!"
     "ssssssssss" + string
   else
     string
